375ee3fe77ebcc293acec3856b8a8bd346cdf021
abdusamad.dusabaev <adbusamad.dusabaev@yandex.ru>
2022-12-25 19:45:01 +0300

    Production Version V1.0

---
+++ additional_functions/config.py
@@ -0,0 +1,26 @@
+# LinkedIn Config
+linkedin_login = "linkedin_login"
+linkedin_password = "linkedin_password"
+linkedin_max_messages_count = 10  # correct data
+
+# Email Config
+email_login = "affiliatemarketing.flexe.io@gmail.com"  # correct data
+email_password = "azmrkahunjfjiceb"  # correct data
+
+# Telegram Config
+api_id = 21802147  # protect data
+api_hash = "6f5afc52d47a7c17a0745a0758bfa30e"  # protect data
+
+# Google Sheet Config
+sheet_id = ""
+start_index = 2  # correct data
+
+# Report Config
+oclock = "20:00"  # correct data
+
+# Database config
+port = 3306
+host = "127.0.0.1"
+db_username = "root"
+password = "password"
+db_name = "db_name"
